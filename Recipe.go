ed714ba437341b1ff6ff21b1007f7cfef4771e1d
fezcode <samil.bulbul@gmail.com>
2026-05-09 23:23:06 +0300

    Expand build targets to linux/darwin/windows on amd64+arm64

--- Recipe.go
+++ Recipe.go
@@ -18,7 +18,12 @@ func Run(bake *gobake.Engine) error {
 			os   string
 			arch string
 		}{
+			{"linux", "amd64"},
+			{"linux", "arm64"},
 			{"windows", "amd64"},
+			{"windows", "arm64"},
+			{"darwin", "amd64"},
+			{"darwin", "arm64"},
 		}
 
 		err := ctx.Mkdir("build")
